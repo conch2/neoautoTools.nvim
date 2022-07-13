8d9c30bc6d3c3f6def85966a4d3b88700c37d4b8
conch <2949859498@qq.com>
2022-07-14 01:08:40 +0800

    测试

---
+++ lua/neoauto/init.lua
@@ -0,0 +1,13 @@
+
+local M = {}
+
+print('neoauto init')
+
+function M.setup(attr)
+    if attr == nil then
+        attr = {}
+    end
+end
+
+return M
+
---
+++ plugin/neoauto.vim
@@ -0,0 +1,9 @@
+
+if exists('g:loaded_conch_neoauto')
+    finish
+endif
+
+command! NeoAutoTAB lua print('NeoAutoTAB')
+
+let g:loaded_conch_neoauto=1
+
