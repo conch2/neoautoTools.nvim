e2e0ad6bd54594face0c10ff94e3cbdab761e61a
conch <2949859498@qq.com>
2022-07-14 16:13:44 +0800

    测试可行性

--- lua/neoautoTools/init.lua
+++ lua/neoautoTools/init.lua
@@ -1,12 +1,26 @@
 
 local M = {}
 
-print('neoauto init')
+local vf = vim.fn
 
+local function setmapping()
+    vim.api.nvim_set_keymap("v", "<C-l>",
+            "<CMD>lua require('neoautoTools').package()<CR>",
+            {noremap=true, silent=true})
+end
+
+require()
+
+function M.package()
+    vim.cmd("normal <ESC>")
+end
+
+-- @argment attr table
 function M.setup(attr)
     if attr == nil then
         attr = {}
     end
+    setmapping()
 end
 
 return M
